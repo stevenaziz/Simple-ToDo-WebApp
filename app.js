e96f1e290fa4cda2861d15eeef60514031784f2a
Steven Aziz <16842077+stevenaziz@users.noreply.github.com>
2022-11-21 17:53:00 -0800

    Update app.js

--- app.js
+++ app.js
@@ -10,18 +10,11 @@ const port = 8080;
 const appName = "Task Manager";
 
 // Middleware
+app.use(express.static("./Client"));
 app.use(express.json());
 
 // Data model (schema)
 const tasks = require("./Task");
-
-// Routes
-// app.get("/tm/tasks")        - get all the tasks
-// app.get("/tm/tasks/:id")    - get a task
-// app.post("/tm/tasks")       - add a new task
-// app.patch("/tm/tasks/:id")  - update a task
-// app.delete("/tm/tasks/:id") - delete a task
-
 
 // get all the tasks
 app.get("/tm/tasks", async (req,res)=>{
@@ -32,9 +25,6 @@ app.get("/tm/tasks", async (req,res)=>{
     res.status(500).json({msg: error});
   };
 });
-
-// get one task
-// use mongoose findOne() function for this
 
 // add a task
 app.post("/tm/tasks", async (req,res)=>{
@@ -48,9 +38,26 @@ app.post("/tm/tasks", async (req,res)=>{
 
 // update a task
 // use mongoose fineOneAndUpdate function for this
+app.updateTask("/tm/tasks/:id", async (req, res) => {
+  try {
+    const task = await tasks.findByIdAndUpdate({ _id: req.params.id }, {name:req.body.name, checked: req.body.checked});
+    res.status(200).send(task);
+   } catch (error) {
+    res.status(404).json({success: false, error}); 
+   }
+  });
+ 
 
 // delete a task
 // use mongoose findOneAndDelete function for this
+app.delete("/tm/tasks/:id", async (req, res) => {
+  try {
+    const task = await tasks.findOneAndDelete({ _id: req.params.id });
+    res.status(200).send("Task was deleted");
+  } catch (error) {
+    res.status(404).json({error});
+  }
+ });
 
 // Connect to the database and start the appl server
 const start = async () => {
