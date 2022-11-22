a5079dbc39b502181c0f0c49bccce03d2491a117
Steven Aziz <16842077+stevenaziz@users.noreply.github.com>
2022-11-22 12:07:35 -0800

    Fixed syntax error

--- client/scripts.js
+++ client/scripts.js
@@ -73,7 +73,7 @@ async function DeleteTask(object) {
     await main();
   });
   cnclBtn.addEventListener("click", async () => {
-    result.innerHTML = currTask;
+    result.innerHTML = currTasks;
   });
 }
 
